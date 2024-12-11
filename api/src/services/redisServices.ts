2e30d58c1ceb8a6a1ee1f68f1efd02ead54492e9
Prasanna Desai <xdeveloper.codex@gmail.com>
2024-12-11 22:44:54 +0530

    Check redis

--- api/src/services/redisServices.ts
+++ api/src/services/redisServices.ts
@@ -1,15 +1,26 @@
-import Redis, {Redis as RedisClient} from 'ioredis'
+import Redis, { Redis as RedisClient } from "ioredis";
 
-const redisClient : RedisClient = new Redis({
-    host: 'localhost',
-    port: 6379
-})
+const redisClient: RedisClient = new Redis({
+  host: "localhost",
+  port: 6379,
+});
 
-export const publishToQueue =  async (queue: string, message: any)=> {
-    try {
-       await redisClient.rpush(queue, JSON.stringify(message)) 
-    } catch (error) {
-        console.log('Error publishing message:')
-        throw new Error('Error publishing message')
-    }
-}
+redisClient
+  .ping()
+  .then((reply) => {
+    console.log("Redis connection successful:", reply);
+  })
+  .catch((error) => {
+    console.error("Redis connection error:", error);
+  });
+
+export const publishToQueue = async (queue: string, message: any) => {
+  try {
+    console.log(queue);
+    console.log(message);
+    await redisClient.rpush(queue, JSON.stringify(message));
+  } catch (error) {
+    console.log("Error publishing message:");
+    throw new Error("Error publishing message");
+  }
+};
