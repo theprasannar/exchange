df417faddb83d334770680b9022288960b35cb6b
theprasannar <xdeveloper.codex@gmail.com>
2025-04-20 18:50:03 +0530

    Env client redis

--- engine/src/index.ts
+++ engine/src/index.ts
@@ -1,40 +1,36 @@
-import { createClient, RedisClientType } from 'redis';
-import { Engine } from './trade/engine';
+import { createClient, RedisClientType } from "redis";
+import { Engine } from "./trade/engine";
 
 async function main() {
-    const engine = new Engine();
-    const redisClient = createClient()
-    redisClient.connect();
-    console.log('Successfully connected')
+  const engine = new Engine();
+  const redisClient = createClient({ url: process.env.REDIS_URL });
+  redisClient.connect();
+  console.log("Successfully connected");
 
-    while (true) {
-        const message = await redisClient.brPop('message', 0);
+  while (true) {
+    const message = await redisClient.brPop("message", 0);
 
-        if (message) {
-            try {
-                // Use object destructuring
-                const { element } = message; 
-                const parsedPayload = JSON.parse(element);
-        
-                const { clientId, message: orderMessage } = parsedPayload;
-        
-                const response = engine.process(parsedPayload);
+    if (message) {
+      try {
+        // Use object destructuring
+        const { element } = message;
+        const parsedPayload = JSON.parse(element);
 
-                if (response ?? false) {
-                    await redisClient.publish(clientId, JSON.stringify(response));
-                } else {
-                }
-                
-        
-        
-            } catch (error) {
-                console.error('Failed to process message:', message, error);
-            }
+        const { clientId, message: orderMessage } = parsedPayload;
+
+        const response = engine.process(parsedPayload);
+
+        if (response ?? false) {
+          await redisClient.publish(clientId, JSON.stringify(response));
         } else {
-            console.warn('No message received.');
         }
-        
+      } catch (error) {
+        console.error("Failed to process message:", message, error);
+      }
+    } else {
+      console.warn("No message received.");
     }
+  }
 }
 
 main();
