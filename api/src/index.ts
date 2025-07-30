11da3a7b6888bfc34272b40518fd44715493febe
Prasanna Desai <xdeveloper.codex@gmail.com>
2025-07-30 22:54:03 +0530

    Health point

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,37 +1,39 @@
-import express from 'express';
-import cors from 'cors';
-import orderRoutes from './routes/orderRoutes'
-import tickerRoutes from './routes/tickerRoutes'
-import tradeRoutes from "./routes/tradeRoutes"; 
-import klineRoutes from "./routes/klineRoutes"; 
+import express from "express";
+import cors from "cors";
+import orderRoutes from "./routes/orderRoutes";
+import tickerRoutes from "./routes/tickerRoutes";
+import tradeRoutes from "./routes/tradeRoutes";
+import klineRoutes from "./routes/klineRoutes";
 import authRoutes from "./routes/authRoutes";
-import userRoutes from './routes/userRoutes';
-import balanceRoutes from './routes/balanceRoutes';
-
-
+import userRoutes from "./routes/userRoutes";
+import balanceRoutes from "./routes/balanceRoutes";
 
 const app = express();
 
-app.use(cors({
-    origin: '*',  // Allows requests from any origin, change '*' to specific domains for security
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization']  // Allowed headers
-  }));
+app.use(
+  cors({
+    origin: "*", // Allows requests from any origin, change '*' to specific domains for security
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  })
+);
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
 
-app.use('/api/v1/orders', orderRoutes);
-app.use('/api/v1/ticker', tickerRoutes);
+app.get("/health", (_req: any, res: any) => {
+  res.send({ status: "ok" });
+});
+
+app.use("/api/v1/orders", orderRoutes);
+app.use("/api/v1/ticker", tickerRoutes);
 app.use("/api/v1/trades", tradeRoutes);
 app.use("/api/v1/klines", klineRoutes);
 app.use("/api/v1/auth", authRoutes);
-app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/balance', balanceRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/balance", balanceRoutes);
 
-
-
-app.listen(PORT, ()=> {
-    console.log(`listening on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
